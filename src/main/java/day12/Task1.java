c720bd98462bfcdbf2e404d6622b8cde65db0cb3
Your Name <you@example.com>
2023-02-13 15:06:08 +0400

    work day8

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,21 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
+
+        arrayList.add("AUDI");
+        arrayList.add("BMW");
+        arrayList.add("LADA");
+        arrayList.add("Porsche");
+        arrayList.add("Peugeot");
+        arrayList.add(arrayList.size()/2,"VOLVO");
+        arrayList.remove(0);
+        System.out.println(arrayList);
 
     }
 }
--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,30 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        List <Integer> array = new ArrayList<>();
+        arraySORT(0,30,array);
+        System.out.println(array);
+
+
+
+
+
+    }
+
+    public  static void arraySORT(int a, int b, List<Integer> array){
+        for (int i = a; i < b+1; i+=2) {
+            array.add(i);
+            if(i>=b){
+                for (int j = 300; j < 351; j+=2) {
+                    array.add(j);
+                }
+            }
+        }
+        
     }
 }
