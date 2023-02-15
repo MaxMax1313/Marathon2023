397f569cb23afb46c751eeedb20c55fadccdaa6d
Your Name <you@example.com>
2023-02-15 17:05:47 +0400

    work day8

---
+++ src/main/java/day13/MessageDatabase.java
@@ -0,0 +1,12 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageDatabase {
+    static List<Message> messages = new ArrayList<>();
+
+    public static void addNewMessage(User u1, User u2, String text){
+
+    }
+}
