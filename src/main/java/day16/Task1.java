8e06717427ddd9b3b2742d8b7ccdf4dc5daacd7c
Your Name <you@example.com>
2023-02-21 18:00:14 +0400

    work day8

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,38 @@
 package day16;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Task1 {
+
+    public static void printResult(File file) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("файл не найден.");
+            ;
+        }
+
+
+        String line = scanner.nextLine();
+        String[] array = line.split(" ");
+        int count = 0;
+        int summ = 0;
+
+        for (String s : array) {
+            summ += Integer.parseInt(s);
+            count++;
+
+        }
+        float d = summ / (float) count;
+        System.out.println(d);
+    }
+
+    public static void main(String[] args) {
+        File file = new File("Test10.txt");
+        printResult(file);
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,22 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("C:\\Users\\Max\\Desktop\\JavaMarathon2021\\file1.txt");
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            writer.print(random.nextInt(100)+1 + " ");
+        }
+
+        Scanner scanner = new Scanner("file1.txt");
+
+
+        writer.close();
     }
 }
